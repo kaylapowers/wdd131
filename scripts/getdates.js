ab8b0919b0880ce42e777b1f6e41cc8984e9150e
kaylapowers <kpowers@byupathway.edu>
2025-08-20 12:58:39 -0600

    wk1 asst coursepage final

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,11 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
 	var currentYear = new Date().getFullYear();
-	document.getElementById('copyright-year').textContent = currentYear;
+	var copyrightYearElement = document.getElementById('copyright-year');
+	if (copyrightYearElement) {
+		copyrightYearElement.textContent = currentYear;
+	}
 });
 
 function updateLastModified() {
@@ -8,7 +13,9 @@ function updateLastModified() {
 	if (lastModifiedElement) {
 		lastModifiedElement.textContent = document.lastModified;
 	}
-}
+window.onload = function() {
+	updateLastModified();
+}};
 
 // Call the function when the page loads
 window.onload = updateLastModified;
